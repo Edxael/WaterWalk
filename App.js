0667dfef5d6d8e8ab6129e428f5dc24edf4d09e2
Edmundo Rubio <edmundo.rubio@chghealthcare.com>
2021-05-12 09:40:33 -0600

    SetUp CompLib RN-Paper

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider, DefaultTheme, Button } from 'react-native-paper';
+import { name as appName } from './app.json';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'aquamarine',
+    accent: 'yellow',
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open app!</Text>
+        <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+          Press me
+        </Button>
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
   );
 }
 
