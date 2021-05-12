51e3fa08bdf2d61ca46cd816d021774035d8d7e9
cadekynaston <cade.kynaston@chghealthcare.com>
2021-05-12 13:34:31 -0600

    Render the NoLogNav component instead of the LogIn component

--- src/01-Views/00-MainNav.js
+++ src/01-Views/00-MainNav.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import LogIn from './01-NoLog/01-LogIn'
+import NoLogNav from './01-NoLog/00-NologNav'
 import Dashboard from './02-YesLog/01-Dashboard'
+
+
+function Home({ navigation }) {
+  return (
+    <Text>Home screen</Text>
+  );
+}
+
+function About({ navigation }) {
+  return (
+    <Text>About screen</Text>
+  );
+}
+const Stack = createStackNavigator();
 
 const MainNavigation = () => {
     let isUserLogIn = false
@@ -11,16 +28,16 @@ const MainNavigation = () => {
         <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
             <Dashboard/>
         </View>
-    ); 
-    }else {
-        return (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-            <LogIn />
-        </View>
     );
+    }else {
+      return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <NoLogNav />
+        </View>
+      );
     }
 
-    
+
 }
 
 export default MainNavigation;
