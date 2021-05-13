6f2d68b97305efeb05ebff0ff994c62a859870e7
cadekynaston <cade.kynaston@chghealthcare.com>
2021-05-13 14:23:31 -0600

    Added forgot password view

--- src/01-Views/01-NoLog/03-ForgotPassword.js
+++ src/01-Views/01-NoLog/03-ForgotPassword.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
+import { TextInput, Button, Title } from 'react-native-paper';
 
-const ForgoPassword = () => {
+const ForgotPassword = () => {
+
+  const [email, setEmail] = useState('')
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        ForgotPassword Comp
-      </Text>
+    <View style={{padding: 10}}>
+      <Title style={{ marginBottom: 10}}>
+          Forgot your password? Enter your email  and we'll help you reset it.
+      </Title>
+
+      <TextInput
+        label="Email"
+        mode="outlined"
+        value={email}
+        onChangeText={text => setEmail(text)}
+        style={{ marginBottom: 10}}
+      />
+
+      <Button
+        style={{paddingTop: 10, paddingBottom: 10}}
+        mode="contained"
+        onPress={() => {
+            console.log('Password Reset Requested:', email)
+      }}>
+        Reset Password
+      </Button>
+
+
     </View>
   );
 }
 
-export default ForgoPassword;
+export default ForgotPassword;
