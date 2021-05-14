f4ff53478767f450c967b176978c58fd063a89e6
David Vanfleet <david.vanfleet@chghealthcare.com>
2021-05-14 16:17:07 -0600

    add scrolling to account page

--- src/01-Views/02-YesLog/02-Account.js
+++ src/01-Views/02-YesLog/02-Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { View, ScrollView, SafeAreaView } from 'react-native';
 import { Button, Headline, TextInput, Snackbar} from 'react-native-paper';
 import NavBar from '../../02-Components/01-NavBar'
 import { connect } from 'react-redux'
@@ -18,76 +18,81 @@ const AccountComp = ({ userData, updateUserData, navigation }) => {
   }, [userData])
 
   return (
-    <View style={{height: '100%'}}>
+    <SafeAreaView style={{ flex: 1, paddingTop: NavBar.currentHeight } }>
       <NavBar
         title={"Account"}
         navigation={navigation}
       />
-      <View style={{ padding: 10}}>
-        <TextInput
-            label="Email"
-            mode="outlined"
-            value={email}
-            style={{ marginBottom: 10}}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, email: text}))}
-          />
-        <TextInput
-            label="First Name"
-            mode="outlined"
-            value={firstName}
-            style={{ marginBottom: 10}}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, firstName: text}))}
-          />
-          <TextInput
-            label="Last Name"
-            mode="outlined"
-            value={lastName}
-            style={{ marginBottom: 10}}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, lastName: text}))}
-          />
-        <TextInput
-            label="Password"
-            mode="outlined"
-            value={passwordVisible ? password : '********'}
-            style={{ marginBottom: 10}}
-            disabled={!passwordVisible}
-            right={<TextInput.Icon icon={passwordVisible ? 'eye' : 'eye-off'} onPress={() => { setPasswordVisible(!passwordVisible) }} />}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, password: text}))}
-        />
-        <TextInput
-            label="Weight (lbs)"
-            mode="outlined"
-            value={weight}
-            style={{ marginBottom: 10}}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, weight: text}))}
-          />
-          <TextInput
-            label="Daily Water Intake Goal (oz)"
-            mode="outlined"
-            value={waterIntakeGoal.toString()}
-            style={{ marginBottom: 10}}
-            onChangeText={text => setLocalUserData((previousData) => ({...previousData, waterIntakeGoal: text}))}
-          />
-        <Button mode="contained"
-          style={{ marginBottom: 10, paddingTop: 10, paddingBottom: 10}}
-          onPress={() => {
-            console.log('Saving user data')
-            updateUserData(localUserData)
-            onToggleSnackBar()
-            setPasswordVisible(false)
-        }}>
-          Save
-        </Button>
 
-      </View>
-        <Snackbar
+      <ScrollView>
+        <View style={{ padding: 10}}>
+          <TextInput
+              label="Email"
+              mode="outlined"
+              value={email}
+              style={{ marginBottom: 10}}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, email: text}))}
+            />
+          <TextInput
+              label="First Name"
+              mode="outlined"
+              value={firstName}
+              style={{ marginBottom: 10}}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, firstName: text}))}
+            />
+            <TextInput
+              label="Last Name"
+              mode="outlined"
+              value={lastName}
+              style={{ marginBottom: 10}}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, lastName: text}))}
+            />
+          <TextInput
+              label="Password"
+              mode="outlined"
+              value={passwordVisible ? password : '********'}
+              style={{ marginBottom: 10}}
+              disabled={!passwordVisible}
+              right={<TextInput.Icon icon={passwordVisible ? 'eye' : 'eye-off'} onPress={() => { setPasswordVisible(!passwordVisible) }} />}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, password: text}))}
+          />
+          <TextInput
+              label="Weight (lbs)"
+              mode="outlined"
+              value={weight}
+              style={{ marginBottom: 10}}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, weight: text}))}
+            />
+            <TextInput
+              label="Daily Water Intake Goal (oz)"
+              mode="outlined"
+              value={waterIntakeGoal.toString()}
+              style={{ marginBottom: 10}}
+              onChangeText={text => setLocalUserData((previousData) => ({...previousData, waterIntakeGoal: text}))}
+            />
+          <Button mode="contained"
+            style={{ marginBottom: 10, paddingTop: 10, paddingBottom: 10}} 
+            onPress={() => {
+              console.log('Saving user data')
+              updateUserData(localUserData)
+              onToggleSnackBar()
+              setPasswordVisible(false)
+          }}>
+            Save
+          </Button>
+
+        </View>
+      </ScrollView>
+
+      <Snackbar
           visible={snackbarVisible}
           onDismiss={onDismissSnackBar}
           duration={1000}
           >
           Saved successfully.
       </Snackbar>
-    </View>
+    
+    </SafeAreaView>
   );
 }
 
