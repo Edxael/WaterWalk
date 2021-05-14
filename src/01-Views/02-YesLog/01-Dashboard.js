a4f9a1a0ce9f13979e677edf1db221b7bc5f0a45
David Vanfleet <david.vanfleet@chghealthcare.com>
2021-05-14 11:37:45 -0600

    Add scrolling to Dashboard

--- src/01-Views/02-YesLog/01-Dashboard.js
+++ src/01-Views/02-YesLog/01-Dashboard.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, SafeAreaView  } from 'react-native';
 import { Button, TextInput } from 'react-native-paper';
 import NavBar from '../../02-Components/01-NavBar'
 import { connect } from 'react-redux'
@@ -24,49 +24,55 @@ const DashboarComp = (props) => {
 
 
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       <NavBar 
       title={"Dashboard"} 
       subTitle={"Account"}
       navigation={props.navigation}
       />
-      <View style={styles.mainContainer}>
-        <Text>
-          According to your measurements you should be drinking {ozFromStore} oz of water each day, today you have drink { props.todayWaterIntake } Oz of water.
-        </Text>
+      <ScrollView>
+        <View style={styles.mainContainer}>
+          <Text>
+            According to your measurements you should be drinking {ozFromStore} oz of water each day, today you have drink { props.todayWaterIntake } Oz of water.
+          </Text>
 
-        <ChartComp title={'Water Intake'} infoData={updatedWaterNumbers} />
+          <ChartComp title={'Water Intake'} infoData={updatedWaterNumbers} />
 
-        <TextInput
-          label="Add Oz of water"
-          mode="outlined"
-          value={waterToAdd}
-          onChangeText={text => setWaterToAdd(text)}
-          keyboardType="numeric"
-          style={{ marginBottom: 10}}
-        />
+          <TextInput
+            label="Add Oz of water"
+            mode="outlined"
+            value={waterToAdd}
+            onChangeText={text => setWaterToAdd(text)}
+            keyboardType="numeric"
+            style={{ marginBottom: 10}}
+          />
 
-        <Button
-          style={{paddingTop: 10, paddingBottom: 10}}
-          mode="contained"
-          onPress={() => {
-              console.log('Adding Water')
-              if(waterToAdd != ''){
-                addWater(parseInt(waterToAdd, 10))
-                setWaterToAdd('')
-              }
-        }}>
-          Add Water
-        </Button>
+          <Button
+            style={{paddingTop: 10, paddingBottom: 10}}
+            mode="contained"
+            onPress={() => {
+                console.log('Adding Water')
+                if(waterToAdd != ''){
+                  addWater(parseInt(waterToAdd, 10))
+                  setWaterToAdd('')
+                }
+          }}>
+            Add Water
+          </Button>
 
-        <ChartComp title={'Walk Steps'} infoData={walkData} />
+          <ChartComp title={'Walk Steps'} infoData={walkData} />
         
-      </View>
-    </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: NavBar.currentHeight,
+  },
   mainContainer: {
     padding: 10
   }
@@ -85,7 +91,3 @@ function mapDispatcherToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatcherToProps)(DashboarComp)
-
-//{ justifyContent: "center", alignItems: "center" }
-
-// if (/^\d+$/.test(text)) {
